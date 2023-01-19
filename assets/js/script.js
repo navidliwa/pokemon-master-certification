a916758c3a47b73d425bfe1e32d4abfc92d8474e
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-01-18 17:54:36 -0700

    js files to start the game, keep track of questions and answers, and to store and display users' high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,91 +13,83 @@ var availableQuestions = []
 var questions = [
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
+        answer: 2,
     },
     {
         question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
         choice4: '17',
-        answer: '2',
-    },
-    {
-        question: 'What are the 3 types of Starter Pokemon?',
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
-        choice4: '17',
-        answer: '2',
+        answer: 2,
     },
 ]
 
@@ -107,10 +99,62 @@ var MAX_QUESTIONS = 10
 startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestions = [...quest]
+    availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = () => {
-    if(availableQuestions)
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        var number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        var selectedChoice = e.target
+        var selectedAnswer = selectedChoice.dataset['number']
+
+        var classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+startGame()
